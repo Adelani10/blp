3630eb69bfa42b4760423231ce03945c2f6942b7
ade <harounadelani@gmail.com>
2024-02-09 07:32:43 +0100

    changes

--- app/components/side.js
+++ app/components/side.js
@@ -3,4 +3,10 @@ import Link from "next/link";
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 
-export default function Hero() {}
+export default function Hero() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
